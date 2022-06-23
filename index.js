de509b6661333430f2ec85a943354f52eaf24c1b
Danil Panarin <danilapanarin96@gmail.com>
2022-06-23 16:32:05 +0600

    controller(registr)

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { validationResult } from 'express-validator'
+
+import { registerValidation } from './validations/auth.validation.js'
+import UserModel from './models/user.models.js'
+
+mongoose
+	.connect(
+		'mongodb+srv://admin:admin123@cluster0.m00tk.mongodb.net/publication-mern?retryWrites=true&w=majority'
+	)
+	.then(() => console.log('Connect to DB'))
+	.catch(err => console.log(`Invalid error DB ${err}`))
 
 const app = express()
 const port = 4444
 app.use(express.json())
 
-app.get('/', (req, res) => {
-	res.send('Hello world!')
-})
+app.post('/auth/register', registerValidation, async (req, res) => {
+	try {
+		const errors = validationResult(req)
 
-app.post('/auth/login', (req, res) => {
-	console.log(req.body)
-	res.json({
-		success: true,
-	})
+		if (!errors.isEmpty()) {
+			return res.status(400).json(errors.array())
+		}
+
+		const password = req.body.password
+		const salt = await bcrypt.genSalt(10)
+		const hash = await bcrypt.hash(password, salt)
+
+		const doc = new UserModel({
+			email: req.body.email,
+			fullName: req.body.fullName,
+			avatarUrl: req.body.avatarUrl,
+			passwordHash: hash,
+		})
+
+		const user = await doc.save()
+
+		const token = jwt.sign(
+			{
+				_id: user._id,
+			},
+			'secret#45$2',
+			{
+				expiresIn: '10d',
+			}
+		)
+
+		const { passwordHash, ...userData } = user._doc
+
+		res.json({
+			...userData,
+			token,
+		})
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({
+			message: 'Упс:) попробутей позже',
+		})
+	}
 })
 
 app.listen(port, err => {
---
+++ validations/auth.validation.js
@@ -0,0 +1,10 @@
+import { body } from 'express-validator'
+
+export const registerValidation = [
+	body('email', 'Не верный формат почты').isEmail(),
+	body('password', 'Пароль должен быть не менее 5 символов').isLength({
+		min: 5,
+	}),
+	body('fullName', 'Укажите имя').isLength({ min: 3 }),
+	body('avatarUrl', 'Не удалось найти ссылку аватар').optional().isURL(),
+]
